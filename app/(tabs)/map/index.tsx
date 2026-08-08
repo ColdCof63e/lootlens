c301bc6adfe72eb9c627b7e637d57a0d811c871c
Narendra B T <narendrabt93@gmail.com>
2026-08-08 18:34:32 -0400

    Integrate interactive map view component

--- app/(tabs)/map/index.tsx
+++ app/(tabs)/map/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native'
+import React from 'react'
+import { View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
 
 export default function MapScreen() {
     return (
-        <View className='flex-1 bg-black items-center justify-center'>
-            <Text className='text-white text-lg font-bold'>Pathfinders Map</Text>
+        <View style={styles.container}>
+            <MapView provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            initialRegion={{
+                latitude: 43.6532,
+                longitude: -79.3832,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+            }}/>
         </View>
     )
+}
+
+const styles = {
+    container: {
+        flex:1,
+        backgroundColor: "#000"
+    },
+
+    map: {
+        flex: 1
+    }
 }
