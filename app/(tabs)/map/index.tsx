7ae3ef4a6bbe26c505aa7dc0076a2e81f5796fb4
Narendra B T <narendrabt93@gmail.com>
2026-08-08 00:45:13 -0400

    Populate base index screens for map, pulse, and profile tabs

---
+++ app/(tabs)/map/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function MapScreen() {
+    return (
+        <View className='flex-1 bg-black items-center justify-center'>
+            <Text className='text-white text-lg font-bold'>Pathfinders Map</Text>
+        </View>
+    )
+}
---
+++ app/(tabs)/pulse/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function PulseScreen() {
+    return (
+        <View className='flex-1 bg-black items-center justify-center'>
+            <Text className='text-white text-lg font-italic'>Pulse Screen View</Text>
+        </View>
+    )
+}
